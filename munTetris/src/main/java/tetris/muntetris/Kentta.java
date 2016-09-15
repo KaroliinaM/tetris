b00a521dbbf59a1ec8e25cc2bcbcd056adc57969
Mietola Karoliina A <kmietola@da2-ex-dk107-10.helsinki.fi>
2016-09-15 16:34:40 +0300

    uusia kansioita

---
+++ munTetris/src/main/java/tetris/muntetris/Kentta.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tetris.muntetris;
+
+/**
+ *
+ * @author kmietola
+ */
+public class Kentta {
+    
+    private int leveys;
+    private int korkeus;
+    
+    public Kentta()
+    {
+        this.leveys=12;
+        this.korkeus=30;
+    }
+    
+}
---
+++ munTetris/src/main/java/tetris/muntetris/Rivi.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tetris.muntetris;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kmietola
+ */
+public class Rivi {
+    private ArrayList<Integer> rivi; 
+    
+    public Rivi()
+    {
+      this.rivi=new ArrayList<Integer>();  
+    }
+    
+    
+}
