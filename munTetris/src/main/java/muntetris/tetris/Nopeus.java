5d435320b9a2174f1d8e91a35d4c72dc9c40cab3
KaroliinaM <karoliina.mietola@helsinki.fi>
2016-10-20 01:05:41 +0300

    uusia luokkia

---
+++ munTetris/src/main/java/muntetris/tetris/Nopeus.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muntetris.tetris;
+
+/**
+ *
+ * @author kape
+ */
+public class Nopeus {
+
+    private int nopeus;
+    private boolean laskettu;
+    private int kierrokset;
+
+    public Nopeus(int alkunopeus, int kierrokset) {
+        this.nopeus = alkunopeus;
+        this.laskettu=true;
+        this.kierrokset=kierrokset;
+
+    }
+    public int getNopeus()
+    {
+        return nopeus;
+    }
+    public void laskeNopeus(int pisteet)
+    {
+        if(laskettu)
+        {
+            if(pisteet%this.kierrokset!=0)
+            {
+               this.laskettu=false;
+               System.out.println("hei");
+            }
+        }
+        else
+        {
+            if(pisteet%this.kierrokset==0)
+            {
+                this.nopeus*=0.75;
+               this.laskettu=true; 
+            }
+        }
+        
+    }
+
+}
---
+++ munTetris/src/test/java/muntetris/tetris/NopeusTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muntetris.tetris;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kape
+ */
+public class NopeusTest {
+    Nopeus nopeus;
+    public NopeusTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        nopeus=new Nopeus(1000);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void
+}
